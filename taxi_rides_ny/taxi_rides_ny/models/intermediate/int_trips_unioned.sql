aa9dae2ab2c2e25faaccafce2dc532c525649319
Mireille El Haddad <45281188+mireillehaddad@users.noreply.github.com>
2026-02-14 06:13:58 +0000

    intermediate changes

---
+++ taxi_rides_ny/taxi_rides_ny/models/intermediate/int_trips_unioned.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='view') }}
+
+with green as (
+    select * from {{ ref('stg_green_tripdata') }}
+),
+yellow as (
+    select * from {{ ref('stg_yellow_tripdata') }}
+)
+
+select * from green
+union all
+select * from yellow
---
+++ taxi_rides_ny/taxi_rides_ny/models/marts/dim_vendors.sql
@@ -0,0 +1,7 @@
+with trips_unioned as (
+    select * from {{ ref('int_trips_unioned') }}
+),
+vendors as (
+    select distinct vendor_id from trips_unioned
+)
+select * from vendors
