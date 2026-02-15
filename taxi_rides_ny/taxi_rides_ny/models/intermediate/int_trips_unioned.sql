3d50199c2023413caa17f63b2705b356637dee1a
Mireille El Haddad <45281188+mireillehaddad@users.noreply.github.com>
2026-02-15 00:09:52 +0000

    models 1

--- taxi_rides_ny/taxi_rides_ny/models/intermediate/int_trips_unioned.sql
+++ taxi_rides_ny/taxi_rides_ny/models/intermediate/int_trips_unioned.sql
@@ -1,12 +1,12 @@
-{{ config(materialized='view') }}
-
-with green as (
+with green_tripdata as (
     select * from {{ ref('stg_green_tripdata') }}
 ),
-yellow as (
+yellow_tripdata as (
     select * from {{ ref('stg_yellow_tripdata') }}
+),
+trips_unioned as (
+    select * from green_tripdata
+    union all
+    select * from yellow_tripdata
 )
-
-select * from green
-union all
-select * from yellow
+select * from trips_unioned 
